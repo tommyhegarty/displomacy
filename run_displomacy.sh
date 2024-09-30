fc0d5f2e0e955aacabf50b6a6cd6a9ee8769dad9
Tommy Hegarty <thomas.ps.hegarty@gmail.com>
2024-09-30 11:01:54 -0400

    main : name not param name

--- run_displomacy.sh
+++ run_displomacy.sh
@@ -2,7 +2,7 @@
 
 # file to run the displomacy bot + game runner code
 
-TOKEN=$(aws ssm get-parameter --parameter-name "/displomacy/token" | jq --raw-output '.Parameter.Value')
+TOKEN=$(aws ssm get-parameter --name "/displomacy/token" | jq --raw-output '.Parameter.Value')
 export TOKEN
 
 echo "starting the displomacy python file"
