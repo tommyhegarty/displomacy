d7443dc48ffd7c9d356a550ba26624b22f4edf74
Tommy Hegarty <thomas.ps.hegarty@gmail.com>
2024-10-08 15:46:54 -0400

    38 : private response

--- src/cogs/responses.py
+++ src/cogs/responses.py
@@ -7,6 +7,9 @@ async def respond(inter: disnake.ApplicationCommandInteraction, message: str):
     print(f'Responding to {user} command in {channel} with message [{message}]')
     await inter.send(content=message)
 
-def respond_privately(inter: disnake.ApplicationCommandInteraction, message: str):
+async def respond_privately(inter: disnake.ApplicationCommandInteraction, message: str):
     user=str(inter.author.id)
     channel=inter.channel_id
+    
+    print(f'Responding to {user} command in {channel} with message [{message}]')
+    await inter.send(content=message, ephemeral=True)
